26bae9ad8d038c5dad19e321c8513a602021693f
Shruti12130 <71483307+Shruti12130@users.noreply.github.com>
2023-07-27 17:04:29 +0530

    Add files via upload

---
+++ Linked List/ReverseLL(Recursive).cpp
@@ -0,0 +1,11 @@
+ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) {
+            return head;
+        }
+        
+        ListNode* temp = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        
+        return temp;
+    }
