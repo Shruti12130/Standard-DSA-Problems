d0668a2ad57b96a61cc51cfa0f30e6318d2a8044
Shruti12130 <71483307+Shruti12130@users.noreply.github.com>
2023-07-27 17:07:36 +0530

    Finding Middle Element of LL
    Use the Hare and Tortoise theorem: Take 2 pointers (slow and fast) and initialize both of them to the head of the linked list. Now keep iterating till both the fast pointer and fast->next pointer are NOT NULL and perform the operation of moving the slow pointer by 1 node and fast pointer by 2 nodes. The moment any of the fast or fast->next pointer becomes NULL, the slow pointer would have reached the middle of the linked list. Return the slow pointer.

---
+++ Linked List/MiddleElementOfLL.cpp
@@ -0,0 +1,13 @@
+ListNode* middleNode(ListNode* head) {
+        if(!head || !head->next){
+            return head;
+        }
+      
+        ListNode* slow = head, *fast = head;
+        
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+}
