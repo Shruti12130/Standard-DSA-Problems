686c6b173b60c644fe05c36820653cf6af322fce
Shruti12130 <71483307+Shruti12130@users.noreply.github.com>
2023-07-27 17:44:58 +0530

    Create BottomViewinBT.cpp
    Same as top view of Binary tree except the fact that instead of inserting line and node into the map as mp.insert({line, node}), we will be directly assigning the node to the line as mp[line] = node. We are doing so because each time we will be encountering any node for any line it will keep updating the value and in the end the bottom node's values would remain.

---
+++ Binary Tree/BottomViewinBT.cpp
@@ -0,0 +1,26 @@
+vector <int> bottomView(Node *root) {
+        vector<int> ans;
+        queue<pair<Node*, int>> q;
+        map<int, Node*> mp;
+        q.push({root, 0});
+        
+        while(!q.empty()){
+            auto temp = q.front();
+            q.pop();
+            Node* node = temp.first;
+            int line = temp.second;
+            
+            mp[line] = node;
+            if(node->left) {
+                q.push({node->left, line-1});
+            }
+            if(node->right) {
+                q.push({node->right, line+1});
+            }
+        }
+        
+        for(auto it: mp) {
+            ans.push_back(it.second->data);
+        }
+        return ans;
+}
