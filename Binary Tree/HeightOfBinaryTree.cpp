6d7c52c4ea5a586dd5de276cb724fb1e19ec184c
Shruti12130 <71483307+Shruti12130@users.noreply.github.com>
2023-07-27 17:32:40 +0530

    Update HeightOfBinaryTree.cpp
    Height is basically: for a root node: 1 + max of its left height, the right height. Adding 1 because the node for which we are calculating height will also be contributing for one extra level of height.

--- Binary Tree/HeightOfBinaryTree.cpp
+++ Binary Tree/HeightOfBinaryTree.cpp
@@ -1,3 +1,9 @@
+/*
+        Height is basically: for a root node: 1 + max of its left height, the right height. 
+        Adding 1 because the node for which we are calculating height will also be 
+        contributing for one extra level of height. 
+*/
+
 int height(struct Node* node){
         if(!node) {
             return 0;
