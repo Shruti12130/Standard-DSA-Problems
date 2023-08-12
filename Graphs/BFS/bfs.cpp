bb37728f179e0fac53fe6c6783baca2d527c9db1
Shruti Jai <71483307+Shruti12130@users.noreply.github.com>
2023-08-12 22:58:03 +0530

    Create bfs.cpp

---
+++ Graphs/BFS/bfs.cpp
@@ -0,0 +1,22 @@
+vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> ans;
+        vector<bool> vis(V, 0);
+        vis[0] = 1;
+        queue<int> q;
+        q.push(0);
+        
+        while(!q.empty()) {
+            int node = q.front();
+            ans.push_back(node);
+            q.pop();
+            
+            for(auto it: adj[node]) {
+                if(!vis[it]) {
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        
+        return ans;
+    }
